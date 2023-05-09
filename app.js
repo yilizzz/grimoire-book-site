c9b9300dff3c4309882e00efea9f7fd3bad3d157
yilizzz <yilizhang3@gmail.com>
2023-05-09 13:31:04 +0200

    path

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.resolve(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "/frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }else{
   app.get("/", (req,res)=>{
